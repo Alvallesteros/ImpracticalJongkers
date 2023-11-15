c6b0ec078c5c5f915b2201f38c124633d9aa2c60
justin <justinreyes13003@gmail.com>
2023-11-15 20:20:30 +0800

    CafeteriaComponent, CafeteriaController, CafeteriaDto rollback

---
+++ NewVendorApp/src/app/rest/controllers/CafeteriaController.java
@@ -0,0 +1,48 @@
+package app.rest.controllers;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import app.components.CafeteriaComponent;
+import app.entities.*;
+
+@Path("/cafeteria")
+public class CafeteriaController {
+
+    @Autowired
+    CafeteriaComponent cafComponent;
+
+    @POST
+    @Path("/add")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Cafeteria addCafeteria(CafeteriaDto cafeteriaDto)
+    {
+        return cafComponent.newCafeteria(cafeteriaDto);
+    }
+
+    @POST
+    @Path("/")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Cafeteria> viewCafeteria()
+    {
+        return cafComponent.viewCafeterias();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Path("/remove")
+    public String removeCafeteria(@FormParam("cafeteriaID") int cafId) {
+        return cafComponent.removeCafeteria(cafId);
+    }
+}
